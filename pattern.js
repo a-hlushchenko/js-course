8cd621d6cec036d5d541979039be82457e405dbd
AntonHlushchenko <glantoshka@gmail.com>
2023-08-11 17:12:48 +0300

    pattern 2

--- pattern.js
+++ pattern.js
@@ -314,7 +314,6 @@ const handler = handlerBuilder.add(new CredentialHandler()).add(new TwoFactorAut
 if (!handler.login('guest', 'admin123')) {
   console.log(`вхід заборонено`);
 } 
-*/
 
 class User {
     constructor(name, messanger) {
@@ -347,3 +346,430 @@ class User {
   const user2 = new User('Anton', EmailMessanger);
   user1.sendMessage('Ваш акаунт успішно зареєстровано');
   user2.sendMessage('Ваш акаунт успішно зареєстровано');
+
+class Composite {
+  comments = [];
+
+  addComment(comment) {
+    this.comments.push(comment);
+  }
+
+  removeComment(comment) {
+    const index = this.comments.indexOf(comment);
+
+    if (index >= 0) {
+      this.comments.splice(index, 1);
+    }
+  }
+}
+
+class Comment extends Composite {
+  constructor(text) {
+    super();
+    this.text = text;
+  }
+
+  display() {
+    console.log(`- Коментар: ${this.text}`);
+
+    for (const comment of this.comments) {
+      comment.display();
+    }
+  }
+}
+
+class Video extends Composite {
+  constructor(title) {
+    super();
+    this.title = title;
+  }
+
+  display() {
+    console.log(`Відео ${this.title}`);
+
+    for (const comment of this.comments) {
+      comment.display();
+    }
+  }
+}
+
+const video = new Video("Навчальне відео");
+
+video.addComment(new Comment("Дуже корисне відео"));
+video.addComment(new Comment("qwerty"));
+video.addComment(new Comment("top"));
+
+video.comments[0].addComment(new Comment(`Відповідь: згоден`));
+
+video.display();
+
+class Category {
+  static #categories = {};
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  static create(name) {
+    if (!this.#categories[name]) {
+      this.#categories[name] = new Category(name);
+    }
+
+    return this.#categories[name];
+  }
+}
+
+class Product {
+  constructor(name, category) {
+    this.name = name;
+    this.category = category;
+  }
+
+  display() {
+    console.log(`Product: ${this.name}, category: ${this.category.name}`);
+  }
+}
+
+const electronics = Category.create("Electronics");
+const book = Category.create("Book");
+const electronics2 = Category.create("Electronics");
+
+const product1 = new Product("iphone", electronics);
+const product2 = new Product("airpods", electronics);
+const product3 = new Product("book", book);
+const product4 = new Product("macbook", electronics2);
+
+product1.display();
+product2.display();
+product3.display();
+product4.display();
+
+const list = [product1, product2, product3, product4].filter(
+  (product) => product.category === electronics
+);
+
+console.log(list);
+
+class CoffeeMachine {
+  prepareCoffee() {
+    this.boilWater();
+    this.grindCoffeeBeans();
+    this.brewCoffee();
+    this.poorIntoCup();
+    this.addIngredients();
+    this.serveCoffee();
+  }
+
+  boilWater() {
+    console.log(``);
+  }
+
+  grindCoffeeBeans() {
+    console.log(`Boiling water...`);
+  }
+
+  brewCoffee() {
+    console.log(`Grinding coffe beans...`);
+  }
+
+  poorIntoCup() {
+    console.log(`pooring into cup...`);
+  }
+
+  addIngredients() {}
+
+  serveCoffee() {
+    console.log(`Servering coffee...`);
+  }
+}
+
+class LatteCoffee extends CoffeeMachine {
+  addIngredients() {
+    console.log(`adding milk...`);
+  }
+}
+
+class CappuccinoCoffee extends CoffeeMachine {
+  addIngredients() {
+    console.log(`adding milk and cocoa...`);
+  }
+}
+
+const latteCoffe = new LatteCoffee();
+latteCoffe.prepareCoffee();
+
+const cappuccinoCoffe = new CappuccinoCoffee();
+latteCcappuccinoCoffeoffe.prepareCoffee();
+
+class TextFile {
+  constructor(name, content) {
+    this.name = name;
+    this.content = content;
+  }
+}
+
+class ImageFile {
+  constructor(name, size) {
+    this.name = name;
+    this.size = size;
+  }
+}
+
+class VideoFile {
+  constructor(name, duration) {
+    this.name = name;
+    this.duration = duration;
+  }
+}
+
+class TextEditor {
+  files = [];
+
+  addFile(file) {
+    this.files.push(file);
+  }
+
+  readTextFile(file) {
+    console.log(
+      `Text file: ${file.name}, file content size: ${file.content.length}`
+    );
+  }
+
+  readImageFile(file) {
+    console.log(`Image file: ${file.name}, file size: ${file.size}kb`);
+  }
+
+  readVideoFile(file) {
+    console.log(`Video file: ${file.name}, file duration: ${file.duration}s`);
+  }
+
+  readFiles() {
+    for (const file of this.files) {
+      if (file instanceof TextFile) {
+        this.readTextFile(file);
+      } else if (file instanceof ImageFile) {
+        this.readImageFile(file);
+      } else if (file instanceof VideoFile) {
+        this.readVideoFile(file);
+      }
+    }
+  }
+}
+
+const textFile = new TextFile("document.txt", "this is text file");
+const imageFile = new ImageFile("image.png", 1024);
+const videoFile = new VideoFile("video.mp4", 60);
+
+const textEditor = new TextEditor();
+
+textEditor.addFile(textFile);
+textEditor.addFile(imageFile);
+textEditor.addFile(videoFile);
+
+textEditor.readFiles();
+
+class PaymentSystem {
+  makePayment(amount) {
+    const convertedAmount = this.convertAmount(amount);
+    console.log(`Make payment: $${convertedAmount}`);
+  }
+
+  convertAmount(amount) {
+    return amount * 1.05;
+  }
+}
+
+class OtherPaymnetSystem {
+  submit(amount) {
+    console.log(`Submiting payment request: $${amount}`);
+  }
+}
+
+class PaymnetAdapter {
+  constructor(paymentSystem) {
+    this.paymentSystem = paymentSystem;
+  }
+
+  makePayment(amount) {
+    const convertedAmount = this.convertAmount(amount);
+    this.paymentSystem.submit(convertedAmount);
+  }
+
+  convertAmount(amount) {
+    return amount * 1.05;
+  }
+}
+
+class Order {
+  constructor(amount) {
+    this.amount = amount;
+
+    if (amount < 100) this.paymentSystem = new PaymentSystem();
+    else this.paymentSystem = new PaymnetAdapter(new OtherPaymnetSystem());
+  }
+
+  makePayment() {
+    return this.paymentSystem.makePayment(this.amount);
+  }
+}
+
+const order1 = new Order(1000);
+const order2 = new Order(10);
+
+order1.makePayment();
+order2.makePayment();
+
+class ShopingCart {
+  constructor(discount) {
+    this.discount = discount;
+  }
+
+  items = [];
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  discount(price) {
+    return price >= 100 ? price * 0.95 : price;
+  }
+
+  calculateTotalprice() {
+    const totalPrice = this.items.reduce(
+      (accum, item) => accum + item.price,
+      0
+    );
+
+    const finalyPrice =
+      totalPrice - this.discount.calculateDiscount(totalPrice);
+
+    return finalyPrice;
+  }
+}
+
+class DiscountStrategy {
+  calculateDiscount(price) {
+    return price;
+  }
+}
+
+class RegularDiscountStrategy {
+  calculateDiscount(price) {
+    return price * 0.1;
+  }
+}
+
+class PremiumDiscountStrategy {
+  calculateDiscount(price) {
+    return price * 0.15;
+  }
+}
+
+class NewCustomerDiscountStrategy {
+  calculateDiscount(price) {
+    return price * 0.05;
+  }
+}
+
+const shoppingCart1 = new ShopingCart(new PremiumDiscountStrategy());
+
+shoppingCart1.addItem({ name: "iphone", price: 20 });
+shoppingCart1.addItem({ name: "macbook", price: 80 });
+
+console.log(shoppingCart1.calculateTotalprice());
+
+class User {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+class UserGroup {
+  users = [];
+
+  addUser(user) {
+    this.users.push(user);
+  }
+}
+
+class UserIterator {
+  #users = null;
+  #currentIndex = 0;
+  constructor(userGroup) {
+    this.#users = userGroup.users;
+  }
+
+  #hasNext() {
+    return this.#currentIndex < this.#users.length;
+  }
+
+  next() {
+    if (this.#hasNext()) {
+      const name = this.#users[this.#currentIndex].name;
+      this.#currentIndex++;
+
+      return name;
+    }
+
+    return null;
+  }
+
+  list() {
+    return this.#users.map((user) => user.name).join(", ");
+  }
+}
+
+const group = new UserGroup();
+
+group.addUser(new User("Anton", "glantoshka@gmail.com", "password1"));
+group.addUser(new User("Danya", "danyaokeys@gmail.com", "password2"));
+
+const iterator = new UserIterator(group);
+console.log(iterator.list());
+*/
+
+class User {
+  constructor(name, chat) {
+    this.name = name;
+    this.chat = chat;
+  }
+
+  sendMessage(message) {
+    console.log(`${this.name} send messagge ${message}`);
+    return this.chat.sendMessagge(this, message);
+  }
+
+  receiveMessage(user, message) {
+    console.log(`${this.name} received message from ${user.name}: ${message}`);
+  }
+}
+
+class Chat {
+  users = [];
+
+  addUser(user) {
+    this.users.push(user);
+  }
+
+  sendMessagge(sender, message) {
+    for (const user of this.users) {
+      if (user !== sender) {
+        user.receiveMessage(sender, message);
+      }
+    }
+  }
+}
+
+const chat = new Chat();
+
+const user1 = new User("Anton", chat);
+const user2 = new User("Danya", chat);
+const user3 = new User("Roma", chat);
+
+chat.addUser(user1);
+chat.addUser(user2);
+chat.addUser(user3);
+
+user2.sendMessage("Hello everyone");
